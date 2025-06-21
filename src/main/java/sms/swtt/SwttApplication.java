7f36cec78d069b371d526cf6c07c2d3444e5930c
tjfakstn <tjfakstn04@gmail.com>
2025-06-21 21:27:29 +0900

    first commit

---
+++ src/main/java/sms/swtt/SwttApplication.java
@@ -0,0 +1,13 @@
+package sms.swtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwttApplication.class, args);
+    }
+
+}
